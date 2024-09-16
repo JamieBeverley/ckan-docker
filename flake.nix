091e554a77ee0972b3408cb7eee675fc03b04d0d
Jamie Beverley <jamie.beverley@hotmail.com>
2024-09-16 08:24:26 -0400

    nix flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem(
+      system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          packages.hello = pkgs.hello;
+          packages.nginx = pkgs.dockerTools.buildImage {
+            name = "nginx";
+            config = { image = "nginx"; };
+          };
+          devShells.default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.docker
+            ];
+          };
+        }
+   );
+}
